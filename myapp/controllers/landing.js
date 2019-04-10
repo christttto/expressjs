26236261ee35541f8f398b2d8cbe8ac7a4914d46
Kris <christttto@berkeley.edu>
2019-04-09 22:37:50 -0700

    vaguely works values of insert not populated

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -1,3 +1,4 @@
+const models = require("../models")
 exports.get_landing = function(req, res, next) {
   res.render("landing", { title: "Express" })
 }
@@ -6,5 +7,9 @@ exports.get_landing = function(req, res, next) {
 //express
 exports.submit_lead = function(req, res, next) {
   console.log("lead email:", req.body.lead_email)
-  res.redirect("/")
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect("/")
+  })
 }
