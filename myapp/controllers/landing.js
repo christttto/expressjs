5e78b4e8c0eac58898b700255535adfeb536fa01
Kris <christttto@berkeley.edu>
2019-04-29 17:43:33 -0700

    testing

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -4,12 +4,18 @@ exports.get_landing = function(req, res, next) {
 }
 //this will render from template
 //"index.pug" used, second is key value pair.
+
 //express
+
+// exports.submit_lead = function(req, res, next) {
+//   console.log("lead email:", req.body.lead_email)
+//   return models.Lead.create({
+//     email: req.body.lead_email
+//   }).then(lead => {
+//     res.redirect("/")
+//   })
+// }
 exports.submit_lead = function(req, res, next) {
   console.log("lead email:", req.body.lead_email)
-  return models.Lead.create({
-    email: req.body.lead_email
-  }).then(lead => {
-    res.redirect("/")
-  })
+  res.redirect("/")
 }
